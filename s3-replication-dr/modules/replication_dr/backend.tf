0484550b34e4d7c414ab1d307a1a9c7d38430417
asmini.mishra <asmini.mishra@gmail.com>
2026-06-26 22:04:50 +0530

    'backend updated'

---
+++ s3-replication-dr/modules/replication_dr/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "project-aibootcamp-2026-infra"
+    key            = "replication-dr/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    
+    # Enable new native locking
+    use_lockfile   = true 
+
+  }
+}
